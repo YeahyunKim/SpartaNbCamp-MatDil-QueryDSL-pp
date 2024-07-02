be8f30037e6464636ad87a331cec7cbbe2bfd331
Yeahyun Kim <insidesy4@gmail.com>
2024-07-02 12:10:49 +0900

    [Chore] - [필수 1 - 좋아요 추가하기] - 단일 음식점 / 댓글 조회 시, 좋아요 개수 반환 처리 #1

--- src/main/java/com/sparta/mat_dil/dto/RestaurantResponseDto.java
+++ src/main/java/com/sparta/mat_dil/dto/RestaurantResponseDto.java
@@ -5,12 +5,20 @@ import lombok.Getter;
 
 @Getter
 public class RestaurantResponseDto {
+    private long restaurantId;
+    private long userId;
+    private String userName;
     private String restaurantName;
     private String description;
+    private long likesCnt;
 
     public RestaurantResponseDto(Restaurant restaurant) {
+        this.restaurantId = restaurant.getId();
+        this.userId = restaurant.getUser().getId();
+        this.userName = restaurant.getUser().getName();
         this.restaurantName = restaurant.getRestaurantName();
         this.description = restaurant.getDescription();
+        this.likesCnt = restaurant.getLikesCnt();
     }
 
 }
